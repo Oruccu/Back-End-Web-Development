24f9298cee2000a4201713c3738ec7c3bd682ad1
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-10-01 13:26:07 +0300

    first commit

---
+++ Week-1/Homework-1/Program.cs
@@ -0,0 +1,4 @@
+﻿// Hello World dersinde öğrendiğiniz bilgiler ışığında Adınızı, Soyadınızı ve Patika Bootcamp'inde olma nedeninizi console ekranına yazdıran c# uygulamasını kodlayınız.
+
+Console.WriteLine("Ayşe Nur ORUÇCU");
+Console.WriteLine("Başarılı bir yazılım mühendis olmak için Patika eğitimine katıldım. ");
