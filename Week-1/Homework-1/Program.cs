40e6d53d90aa263459b9e7abc72518a2b3e2ec30
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-10-01 13:32:46 +0300

    Reason for being on the Patika changed

--- Week-1/Homework-1/Program.cs
+++ Week-1/Homework-1/Program.cs
@@ -1,4 +1,7 @@
 ﻿// Hello World dersinde öğrendiğiniz bilgiler ışığında Adınızı, Soyadınızı ve Patika Bootcamp'inde olma nedeninizi console ekranına yazdıran c# uygulamasını kodlayınız.
 
 Console.WriteLine("Ayşe Nur ORUÇCU"); //Console writes the string expression in “”
-Console.WriteLine("Başarılı bir yazılım mühendis olmak için Patika eğitimine katıldım. ");
+Console.WriteLine("Patika 'nin uzman kadrosu ve tecrübeli egitmenleri sayesinde yazilim dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim!");
+
+
+
