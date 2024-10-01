5e1f4ddb77e40ab7f57a46a95a567c04308c2517
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-10-01 13:30:29 +0300

    add message

--- Week-1/Homework-1/Program.cs
+++ Week-1/Homework-1/Program.cs
@@ -1,4 +1,4 @@
 ﻿// Hello World dersinde öğrendiğiniz bilgiler ışığında Adınızı, Soyadınızı ve Patika Bootcamp'inde olma nedeninizi console ekranına yazdıran c# uygulamasını kodlayınız.
 
-Console.WriteLine("Ayşe Nur ORUÇCU");
+Console.WriteLine("Ayşe Nur ORUÇCU"); //Console writes the string expression in “”
 Console.WriteLine("Başarılı bir yazılım mühendis olmak için Patika eğitimine katıldım. ");
