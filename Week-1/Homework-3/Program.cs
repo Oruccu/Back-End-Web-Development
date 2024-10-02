db4dc59d44c3d44cc8908797d65b36d3fbae06e8
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-10-02 10:33:10 +0300

    add Homework-3

---
+++ Week-1/Homework-3/Program.cs
@@ -0,0 +1,31 @@
+﻿int NumberControl; // Variable to hold the number entered by the user
+
+while (true) //Loop
+{
+    Console.WriteLine("Bir sayi giriniz: ");
+    string number = Console.ReadLine();
+
+    // Check if the input is a valid integer
+    if (int.TryParse(number, out NumberControl))
+    {
+        // If the number is greater than 10, print a message
+        if (NumberControl > 10)
+        {
+            Console.WriteLine("Girilen sayi 10'dan büyüktür.");
+        }
+        // If the number is even, print a message
+        if (NumberControl % 2 == 0)
+        {
+            Console.WriteLine("Girilen sayi çifttir.");
+
+        }
+        break; //Loop break 
+    }
+
+    else
+    {
+         // Print an error message for invalid input
+        Console.WriteLine("Lütfen bir sayi giriniz: ");
+    }
+}
+
