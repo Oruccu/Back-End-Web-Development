79234cee2c4f3a5bc33b54c68d03a3de744683f5
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-10-02 12:46:03 +0300

    comment added

--- Week-1/Homework-4/Program.cs
+++ Week-1/Homework-4/Program.cs
@@ -1,33 +1,19 @@
-﻿int Apple = 2;
+﻿// Define the fruit prices
+int Apple = 2;
 int Pear = 3;
 int Strawberry = 2;
 int Banana = 3;
 int Other = 4;
 
+// Welcome message to the user
 Console.WriteLine("Rüya Manavina Hos Geldiniz!");
 Console.Write("Hangi meyveyi satin almak istersiniz? (Elma/Armut/Çilek/Muz/Diğer) ");
 
+// Get the user's selection
 string Choose = Console.ReadLine();
 string ChooseToLower = Choose.ToLower();
 
-//Console.WriteLine(ChooseToLower);
-
-/*
-if(ChooseToLower == "elma") 
-    Console.Write($"Seçtiğiniz meyvenin fiyati: {Apple} TL");
-else if (ChooseToLower == "armut")
-    Console.Write($"Seçtiğiniz meyvenin fiyati: {Pear} TL");
-else if (ChooseToLower == "çilek")
-    Console.Write($"Seçtiğiniz meyvenin fiyati: {Strawberry} TL");
-else if (ChooseToLower == "muz")
-    Console.Write($"Seçtiğiniz meyvenin fiyati: {Banana} TL");
-else if (ChooseToLower == "diğer")
-    Console.Write($"Seçtiğiniz meyvenin fiyati: {Other} TL");
-else
-    Console.Write("Seçtiğiniz meyve Rüya Mavavin'da bulunmamaktadır.");
-
-*/
-
+//switch case used
 switch (ChooseToLower)
 {
     case "elma":
@@ -49,6 +35,25 @@ switch (ChooseToLower)
         Console.WriteLine("Seçtiğiniz meyve Rüya Mavavin'da bulunmamaktadır.");
         break;
 }
+
+
+//if else used
+/*
+if(ChooseToLower == "elma") 
+    Console.Write($"Seçtiğiniz meyvenin fiyati: {Apple} TL");
+else if (ChooseToLower == "armut")
+    Console.Write($"Seçtiğiniz meyvenin fiyati: {Pear} TL");
+else if (ChooseToLower == "çilek")
+    Console.Write($"Seçtiğiniz meyvenin fiyati: {Strawberry} TL");
+else if (ChooseToLower == "muz")
+    Console.Write($"Seçtiğiniz meyvenin fiyati: {Banana} TL");
+else if (ChooseToLower == "diğer")
+    Console.Write($"Seçtiğiniz meyvenin fiyati: {Other} TL");
+else
+    Console.Write("Seçtiğiniz meyve Rüya Mavavin'da bulunmamaktadır.");
+
+*/
+
 
 
 
