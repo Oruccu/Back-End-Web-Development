d91493fa2aa9b72cab147f8199e9f192d606a1a3
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-10-04 18:09:11 +0300

    test

--- Week-2/Homework-4/obj/Debug/net8.0/Homework-4.AssemblyInfo.cs
+++ Week-2/Homework-4/obj/Debug/net8.0/Homework-4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0659a739b56159df99d7a4c6f7d07294844e50ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af3ccb1c1bf0b0dacb8fe0b75a52b77a3f4adadb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
