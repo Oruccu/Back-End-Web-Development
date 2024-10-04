90120ace4ebe172e3ef71dc976246837755436bb
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-10-04 09:44:22 +0300

    add while and do-while comparison

--- Week-2/Homework-3/Program.cs
+++ Week-2/Homework-3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int counter = 0;
+int counter2 = 0;
+string limit = Console.ReadLine();
+int InputLimit = Convert.ToInt32(limit);
+
+#region 1.Method
+while(true){
+    counter++;
+    if(counter<=InputLimit){
+        Console.WriteLine(" Ben bir Patika'lıyım ");
+    }
+    else
+        break;
+}
+#endregion
+Console.WriteLine("------------------------");
+#region 2.Method
+do
+{
+    counter2++;
+    if(counter2<=InputLimit){
+        Console.WriteLine(" Ben bir Patika'lıyım ");
+    }
+    else
+        break;
+} while (true);
+
+#endregion
+
+//Comparison While - Do While
+//The while loop may not run at all if the condition is not met. The do-while loop runs at least once.
