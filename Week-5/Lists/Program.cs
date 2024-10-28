7f0aa116981bd4142bfb32deb7b64d9a8ae89266
oruccu <oruccu.ayse1999@gmail.com>
2024-10-28 14:59:02 +0300

    lists homework completed

---
+++ Week-5/Lists/Program.cs
@@ -0,0 +1,13 @@
+﻿List<string> Name = new List<string>();
+Name.Add("Bülent Ersoy");
+Name.Add("Ayşe Nur");
+Name.Add("Ebru Gündeş");
+Name.Add("Hadise");
+
+Console.WriteLine("** Davetliler **");
+int i = 1;
+foreach (string item in Name)
+{
+    Console.WriteLine($"{i} - {item} ");
+    i = 1 + i;
+}
