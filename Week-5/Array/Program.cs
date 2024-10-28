bf20323c1e3c7e636b017e485cc640fae55177d2
oruccu <oruccu.ayse1999@gmail.com>
2024-10-28 14:31:02 +0300

    assignment completed

---
+++ Week-5/Array/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Lütfen {i + 1}. değeri giriniz. ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Mevcut dizi elemanları:");
+foreach (int i in array)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine(" ");
+
+int[] newarray = new int[array.Length + 1];
+for (int i = 0; i < array.Length;i++){
+    newarray[i] = array[i];
+}
+
+Console.WriteLine("Lütfen 11. değeri giriniz");
+int newNumber = Convert.ToInt32(Console.ReadLine());
+newarray[newarray.Length-1] = newNumber;
+
+Array.Sort(newarray); //array small to large rows
+Array.Reverse(newarray);
+
+Console.WriteLine("Büyükten küçüğe sıralanmış yeni dizi elemanları:");
+foreach(int i in newarray){
+    Console.Write(i + " ");
+}
