04d4ffacddd9577c721531d26733de1ae67f4ced
oruccu <oruccu.ayse1999@gmail.com>
2024-10-28 20:43:21 +0300

    Try-Catch Homework completed

---
+++ Week-5/Try-Catch/Program.cs
@@ -0,0 +1,19 @@
+﻿while (true)
+{
+
+    try
+    {
+        Console.Write("Bir sayı giriniz: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Girmiş olduğunuz sayının karesi: " + number * number);
+        break;
+    }
+    catch (OverflowException ex)
+    {
+        Console.WriteLine("Çok büyük yada çük küçük bir sayı girdiniz" + ex.Message);
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Lütfen girdiğiniz değeri kontrol ediniz." + ex.Message);
+    }
+}
