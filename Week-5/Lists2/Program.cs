7b70abd474ed68e161275c8b4f70b8755acf20dc
oruccu <oruccu.ayse1999@gmail.com>
2024-10-28 15:28:59 +0300

    Lists 2 assignment completed

---
+++ Week-5/Lists2/Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> Coffee = new List<string>();
+for (int i = 0;i < 5; i++){
+    Console.Write($"Kahve {i+1}. ");
+    Coffee.Add(Console.ReadLine());
+}
+
+Console.WriteLine("Girilen Kahve İsimleri");
+foreach (var item in Coffee)
+{
+    Console.WriteLine(item);
+}
