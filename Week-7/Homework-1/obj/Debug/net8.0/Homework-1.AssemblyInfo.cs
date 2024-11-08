023227acd50bbef6dba6d6c3b12c0d4618b401ea
oruccu <oruccu.ayse1999@gmail.com>
2024-11-08 11:34:23 +0300

    linq first homework completed

--- Week-7/Homework-1/obj/Debug/net8.0/Homework-1.AssemblyInfo.cs
+++ Week-7/Homework-1/obj/Debug/net8.0/Homework-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d246432b275d81c5a3ab64ccabe981b172bee28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f41cd17dc02fabf90fcc228b1e23b0576a24316")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
